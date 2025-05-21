3c4ae7326602bb8904050a2eb2cd07aebca31245
maykon <maykon.mendes40@gmail.com>
2025-05-21 08:49:17 -0300

    a

--- login.php
+++ login.php
@@ -8,26 +8,37 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
           integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
           crossorigin="anonymous" defer></script>
 </head>
 <body class="bg-light">
 
   <div class="container d-flex justify-content-center align-items-center min-vh-100 mt-5">
-    <div class="card shadow-sm p-4" style="width: 100%; max-width: 400px;">
-      <h4 class="mb-4 text-center">Pagina de login</h4>
-      <form>
-        <div class="form-group">
-          <label for="username">Nome de Usuário</label>
-          <input type="text" class="form-control" id="username" placeholder="Seu nome de usuário">
-        </div>
-        <div class="form-group">
-          <label for="password">Senha</label>
-          <input type="password" class="form-control" id="password" placeholder="Senha">
-        </div>
-        <button type="submit" class="btn btn-outline-primary btn-block">Entrar</button>
-      </form>
+    <div class="card shadow-sm p-0" style="width: 100%; max-width: 400px;">
+      
+      <header class="bg-dark text-white p-4 text-center">
+        <h2>Página de Login</h2>
+      </header>
+
+      <div class="p-4">
+        <form>
+          <div class="form-group">
+            <label for="username">Nome de Usuário</label>
+            <input type="text" class="form-control" id="username" placeholder="Seu nome de usuário">
+          </div>
+          <div class="form-group">
+            <label for="password">Senha</label>
+            <input type="password" class="form-control" id="password" placeholder="Senha">
+          </div>
+          <button type="submit" class="btn btn-outline-dark btn-block">Entrar</button>
+        </form>
+      </div>
+
+      <footer class="bg-dark text-white p-3 text-center">
+        Todos os direitos reservados.
+      </footer>
+
     </div>
   </div>
 
