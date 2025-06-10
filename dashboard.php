8f38422556dfcdfed4bed3b82e02ad7abc0ea3ba
Maykon <maykon.mendes40@gmail.com>
2025-06-10 19:53:58 -0300

    s

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.php?return=Acesso não autorizado. Faça login.");
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>Dashboard</title>
   <link rel="stylesheet" href="bootstrap.css">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -19,17 +24,15 @@ session_start();
 <body>
 <div class="container mt-2">
     <div class="row justify-content-center">
-    <div class="col-12 col-sm-8 col-md-6">
-        <div class="alert alert-success">
-            <h3 class="text-center">
-             Dashboard do Sistema
-             <?php
-             echo "<h4 class='alert-heading mt-3 mr-5 ml-5 text-center'>Seja bem vindo ao sistema ". $_SESSION['usuario'] ."!</h4>";
-             ?>
-            </h3>
+        <div class="col-12 col-sm-8 col-md-6">
+            <div class="alert alert-success">
+                <h3 class="text-center">Dashboard do Sistema</h3>
+                <h4 class="alert-heading mt-3 text-center">
+                    Seja bem-vindo ao sistema <?php echo $_SESSION['usuario']; ?>!
+                </h4>
+            </div>
         </div>
     </div>
 </div>
-
 </body>
 </html>
